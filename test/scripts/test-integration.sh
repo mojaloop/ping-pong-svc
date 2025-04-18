2c0a723bd0211f8ab1b4001c91fcdf040c43f31e
Kevin Leyow <kleyow@gmail.com>
2025-04-18 14:45:58 -0500

    chore: int tests

--- test/scripts/test-integration.sh
+++ test/scripts/test-integration.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+set -euxo pipefail
 
-echo "--=== Running Functional Test Runner ===--"
-exit 0
+source ./docker/env.sh
+
+#docker load -i /tmp/docker-image.tar
+
+docker-compose up -d
+
+echo "==> running integration tests"
+INTEGRATION_TEST_EXIT_CODE=0
+npm run test:int || INTEGRATION_TEST_EXIT_CODE="$?"
+echo "==> integration tests with fspiop adapter exited with code: $INTEGRATION_TEST_EXIT_CODE"
+
+exit $INTEGRATION_TEST_EXIT_CODE
